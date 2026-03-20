a0fcb3dc666c035a49736fc9c78d3e9f5ac8e7f4
ioverlanddogs <ioverlanddogs@gmail.com>
2026-03-20 08:58:48 +0000

    Remove unused admin venue redirect stubs

--- app/(admin)/admin/venue-generation/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function AdminVenueGenerationRedirect() {
-  redirect("/admin/ingest/venue-generation");
-}
--- app/(admin)/admin/venue-images/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function AdminVenueImagesRedirect() {
-  redirect("/admin/ingest/venue-images");
-}
